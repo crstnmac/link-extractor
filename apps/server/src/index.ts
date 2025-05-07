e0cfa56b549a491f5d4272d01b0f4b10557645c7
crstnmac <criston2011@gmail.com>
2025-05-07 13:46:57 +0530

    fix imports

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,7 @@
 import "dotenv/config";
-import { RPCHandler } from "@orpc/server/fetch";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
-import { createContext } from "./lib/context";
 import { prettyJSON } from "hono/pretty-json";
 import { setupStaticFiles } from "./middleware/static";
 import { ApiError, isError } from "./lib/errors";
